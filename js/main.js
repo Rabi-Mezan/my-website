b5548c7aa620853c024afc979afaba5ccdf9f9f8
Rabi-Mezan <ravimezan@gmail.com>
2021-09-21 23:25:31 +0600

    pages added

--- js/main.js
+++ js/main.js
@@ -1,39 +1,20 @@
-document.getElementById('resume').addEventListener('click', function (event) {
-    const detailePage = document.getElementById('details-page');
-    detailePage.innerHTML = `
-   <h2 class="page-title">About Me</h2>
-   <hr class="my-4 hr-line">
-   <ul class="about-me">
-       <li>
-           <p>Full Name : <span>Rabiul Islam</span></p>
-       </li>
-       <li>
-           <p>Birthday : <span>10 NOV, 1998</span></p>
-       </li>
-       <li>
-           <p>Job : <span>Junior Developer</span></p>
-       </li>
-       <li>
-           <p>Website : <span>www.devrabi.com</span></p>
-       </li>
-       <li>
-           <p>Email : <span>ravimezan@gmail.com</span></p>
-       </li>
-       <li>
-           <p>Nationality : <span>Bangladeshi</span></p>
-       </li>
-   </ul>
-   `
-})
-document.getElementById('about').addEventListener('click', function (event) {
-    const detailePage = document.getElementById('details-page');
-    detailePage.innerHTML = `
-    <div class="details-page w-full bg-white mt-14 shadow-md" id="details-page">
+
+
+const detailsPage = document.getElementById('details-page');
+
+function getPageName(id) {
+    const pageName = document.getElementById(id);
+    return pageName;
+}
+
+const aboutPage = getPageName('about');
+aboutPage.addEventListener('click', function (event) {
+    detailsPage.innerHTML = `
     <h2 class="page-title">About Me</h2>
     <hr class="my-4 hr-line">
     <ul class="about-me">
         <li>
-            <p>Full Name : <span>Rabiul Islam</span></p>
+            <p>Name : <span>Rabiul Islam</span></p>
         </li>
         <li>
             <p>Birthday : <span>10 NOV, 1998</span></p>
@@ -53,7 +34,7 @@ document.getElementById('about').addEventListener('click', function (event) {
     </ul>
     <hr class="my-4 hr-line">
     <div class="note">
-        <h2 class="">I'm Junior Developer </h2>
+        <h2 class="page-title">I'm Junior Developer </h2>
         <p>When, while the lovely valley teems with vapour around me, and the meridian sun strikes the
             upper
             surface of the impenetrable foliage of my trees, and but a few stray gleams steal into the
@@ -78,6 +59,122 @@ document.getElementById('about').addEventListener('click', function (event) {
             mirror of my soul, as my soul is the mirror of the infinite God!
         </p>
     </div>
-</div>
-   `
+    <!-- my skill progressbar -->
+    <div class="mt-4 skills">
+        <h2 class="page-title text-center">Professional Skills</h2>
+        <div class="container mt-2">
+
+            <!-- Creating a ui-widgets classes that  
+                store other useful classes like  
+                ui-values and ui-labels -->
+            <div class="ui-widgets html">
+                <div class="ui-values">85%</div>
+                <div class="ui-labels">HTML</div>
+            </div>
+
+            <div class="ui-widgets css">
+                <div class="ui-values">85%</div>
+                <div class="ui-labels">CSS</div>
+            </div>
+            <div class="ui-widgets bootstarp">
+                <div class="ui-values">90%</div>
+                <div class="ui-labels">Bootsrtap</div>
+            </div>
+            <div class="ui-widgets js">
+                <div class="ui-values">70%</div>
+                <div class="ui-labels">JS</div>
+            </div>
+            <div class="ui-widgets react">
+                <div class="ui-values">40%</div>
+                <div class="ui-labels">React</div>
+            </div>
+
+            <div class="ui-widgets tailwind">
+                <div class="ui-values">80%</div>
+                <div class="ui-labels">Tailwind</div>
+            </div>
+
+        </div>
+    </div>
+    <!-- working are -->
+    <h2 class="page-title text-center">Working Area</h2>
+    <div class="working-area my-12">
+        <div class="single-area">
+            <span> <i class="fab fa-apple"></i></span>
+            <h3>Application</h3>
+            <p class="">I work with IOS </p>
+        </div>
+        <div class="single-area">
+            <span> <i class="fas fa-desktop"></i></span>
+            <h3>Web Design</h3>
+            <p>I design website </p>
+        </div>
+        <div class="single-area">
+            <span><i class="fas fa-image"></i></span>
+            <h3>Illustration</h3>
+            <p>I can make vector illustration </p>
+        </div>
+        <div class="single-area">
+            <span><i class="fas fa-code"></i></span>
+            <h3>Development</h3>
+            <p>I am front-end developer </p>
+        </div>
+        <div class="single-area">
+            <span><i class="fas fa-camera"></i></span>
+            <h3>Photography</h3>
+            <p>I love photography </p>
+        </div>
+
+    </div>
+
+    <hr class="my-4 hr-line">
+
+    <footer>
+        <div class="text-center text-xs pb-8">
+            <small>Copyright © /devrabi/ 2021 All right reserved</small>
+        </div>
+    </footer>
+
+    `
+})
+
+const resumePage = getPageName('resume');
+resumePage.addEventListener('click', function (event) {
+
+    detailsPage.innerHTML = `
+    <h2 class="page-title text-center">My Resume</h2>
+    <p class="text-2xl pb-8 text-center text-yellow-500">This page is under construction</p>
+    
+    `
+
+})
+const portfolioPage = getPageName('portfolio');
+portfolioPage.addEventListener('click', function (event) {
+
+    detailsPage.innerHTML = `
+    <h2 class="page-title text-center">My Portfolio</h2>
+    <p class="text-2xl pb-8 text-center text-yellow-500">This page is under construction</p>
+    
+    `
+
+})
+const blogPage = getPageName('blog');
+blogPage.addEventListener('click', function (event) {
+
+    detailsPage.innerHTML = `
+    <h2 class="page-title text-center">My Blogs</h2>
+    <p class="text-2xl pb-8 text-center text-yellow-500">This page is under construction</p>
+    
+    `
+
+})
+const contactPage = getPageName('contact');
+contactPage.addEventListener('click', function (event) {
+
+    detailsPage.innerHTML = `
+    <h2 class="page-title text-center">My Contact</h2>
+    <p class="text-2xl pb-8 text-center text-yellow-500">This page is under construction</p>
+    
+    `
+
 })
